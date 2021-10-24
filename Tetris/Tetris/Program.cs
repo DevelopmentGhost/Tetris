0b35f5c331824276df860bd8c0d618a1cc98ed97
Yevhenii Slyusar <slusarj23@gmail.com>
2021-10-24 16:47:39 +0300

    Створив додаток для Тетрісу.

---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start development application - Tetris.");
+            Console.ReadLine();
+        }
+    }
+}
